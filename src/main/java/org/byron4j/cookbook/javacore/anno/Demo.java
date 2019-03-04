4502313375b3fa5fb04e16f719469bdae2e97049
Byron4j <1552546443@qq.com>
2019-03-04 20:45:10 +0800

    Java核心-注解（一）

---
+++ src/main/java/org/byron4j/cookbook/javacore/anno/Demo.java
@@ -0,0 +1,11 @@
+package org.byron4j.cookbook.javacore.anno;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Demo {
+}
